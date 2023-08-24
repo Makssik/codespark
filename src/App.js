c7d6612fe2953d1e20a5eb17f5922d9fe26e2dc7
Makssik <maksik07052004@gmail.com>
2023-08-24 12:17:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './style.scss'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomeView from './components/views/HomeView';
+import PriceView from './components/views/PriceView';
+import AboutView from './components/views/AboutView';
+import ContactView from './components/views/ContactView';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomeView />} />
+        <Route path="pricing" element={<PriceView />} />
+        <Route path="about" element={<AboutView />} />
+        <Route path="contact" element={<ContactView />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/views/HomeView/HomeView.jsx
@@ -0,0 +1,12 @@
+const HomeView = () => {
+    return (
+        
+        <>
+            <h1>HomeView</h1>
+            <button className="button primery">fdsfsd</button>
+            <button className="button secondary">fdsfsd</button>
+        </>
+    )
+}
+ 
+export default HomeView;
