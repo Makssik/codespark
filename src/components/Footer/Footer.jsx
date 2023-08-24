26928ab010c39931f9b899bf9e837256906c35d4
Makssik <maksik07052004@gmail.com>
2023-08-24 12:25:53 +0300

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <ul className="footer__list">
+          <li>
+            <ul>
+              <li className="logo">CodeSpark</li>
+              <li>
+                2023 © <br /> CodeSpark: Your start in coding!
+              </li>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              <li>Home</li>
+              <li>Pricing</li>
+              <li>About us</li>
+              <li>Contact</li>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              <li>Facebook</li>
+              <li>Instagram</li>
+              <li>Twitter</li>
+              <li>Linkedin</li>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              <li>Privacy Policy</li>
+              <li>Terms Of Service</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
