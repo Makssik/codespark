966805fed758ec74a2e55e7b63689c6f7b9518ac
Makssik <maksik07052004@gmail.com>
2023-08-27 11:26:53 +0300

    create slider for priceList

--- src/components/views/PriceView/PriceView.jsx
+++ src/components/views/PriceView/PriceView.jsx
@@ -1,10 +1,13 @@
 import QuesAndAns from '../../QuesAndAns';
 import PricesList from '../../PricesList';
+import SliderPricesList from '../../PricesList/SliderPricesList';
 
 const PriceView = () => {
+  const isMobile = window.matchMedia('(max-width: 768px)').matches;
   return (
     <>
-      <PricesList />
+      {isMobile ? <SliderPricesList /> : <PricesList />}
+
       <QuesAndAns />
     </>
   );
