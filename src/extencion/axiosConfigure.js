e17bc22f95f0cd1f7b64dbe167f7b75e5ca19ef7
Makssik <maksik07052004@gmail.com>
2023-09-01 15:57:47 +0300

    create contact form, release form submit

---
+++ src/extencion/axiosConfigure.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://codespark-server.onrender.com/api'
+
+const TEST_URL = 'http:localhost:7777'
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default instance
