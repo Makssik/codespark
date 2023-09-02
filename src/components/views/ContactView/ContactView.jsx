7490426432388e677098f19c69cb45d4fcee4012
Makssik <maksik07052004@gmail.com>
2023-09-02 09:45:10 +0300

    add phone number field

--- src/components/views/ContactView/ContactView.jsx
+++ src/components/views/ContactView/ContactView.jsx
@@ -1,10 +1,20 @@
 import ContactForm from '../../ContactForm';
+import ContactUsImg from '../../../images/contact-us.jpg';
 
 const ContactView = () => {
   return (
-    <>
-      <ContactForm />
-    </>
+    <section className="contact">
+      <div className="container">
+        <h2 className="title">Зв'яжіться з нами</h2>
+        <p className="subtitle">
+          Досліджуйте майбутнє разом з нами. Не соромтеся зв'язатися.
+        </p>
+        <div className="img-wrapper">
+          <img src={ContactUsImg} width={400} alt="" />
+        </div>
+        <ContactForm />
+      </div>
+    </section>
   );
 };
 
