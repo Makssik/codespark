ab2082d71bf48b70dfe2c907f8f4f394b60e9774
Makssik <maksik07052004@gmail.com>
2023-09-02 12:57:47 +0300

    ...

--- src/extension/axiosConfigure.js
+++ src/extension/axiosConfigure.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://codespark-server.onrender.com/api'
+const BASE_URL = 'https://codespark-server.onrender.com/api'
 
-const BASE_URL = 'http://localhost:7777/api';
+// const BASE_URL = 'http://localhost:7777/api';
 
 const instance = axios.create({
   baseURL: BASE_URL,
