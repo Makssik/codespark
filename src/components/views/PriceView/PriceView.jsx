35fd37a3716ef374cfda78c98ac126049e47c55d
Makssik <maksik07052004@gmail.com>
2023-09-11 10:11:56 +0300

    add road map
    need fix on future for mobile view

--- src/components/views/PriceView/PriceView.jsx
+++ src/components/views/PriceView/PriceView.jsx
@@ -1,13 +1,18 @@
 import QuesAndAns from '../../QuesAndAns';
 import PricesList from '../../PricesList';
 import SliderPricesList from '../../PricesList/SliderPricesList';
+import RoadMapImg from '../../../images/road-map.png';
 
 const PriceView = () => {
   const isMobile = window.matchMedia('(max-width: 768px)').matches;
   return (
     <>
       {isMobile ? <SliderPricesList /> : <PricesList />}
-
+      <section className='road-map__section'>
+        <div className="container">
+          <img width={'100%'} src={RoadMapImg} alt="" />
+        </div>
+      </section>
       <QuesAndAns />
     </>
   );
