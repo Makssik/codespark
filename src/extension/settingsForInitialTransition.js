02d7256c8e555532baa3c67d1de2e1413bd6a825
Makssik <maksik07052004@gmail.com>
2023-09-11 09:49:29 +0300

    Update settingsForInitialTransition.js

--- src/extension/settingsForInitialTransition.js
+++ src/extension/settingsForInitialTransition.js
@@ -2,7 +2,7 @@ const setings = {
   initial: { opacity: 0, y: '100px' },
   whileInView: { opacity: 1, y: '0' },
   transition: { duration: 0.5, delay: 0.5 },
-  viewport: { once: true, amount: 0.45 },
+  viewport: { once: true, amount: 0.35 },
 };
 
 export default setings
