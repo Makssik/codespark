2bf53891984d0220b49eb228c816853c0f445b9d
Makssik <maksik07052004@gmail.com>
2023-09-25 18:55:10 +0300

    change contact-view

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,6 @@
 import ContactForm from '../ContactForm/ContactForm';
+import SocialsList from '../SocialsList';
 
-import { AiFillInstagram, AiFillYoutube } from 'react-icons/ai';
-import {
-  BiLogoFacebookSquare,
-  BiLogoLinkedinSquare,
-  BiLogoTiktok,
-} from 'react-icons/bi';
 const Footer = () => {
   return (
     <footer id="footer">
@@ -25,38 +20,7 @@ const Footer = () => {
               </a>
             </li>
           </ul>
-          <ul className="footer__socials">
-            <li>
-              {' '}
-              <a href="https://instagram.com/codesparkua?igshid=OGQ5ZDc2ODk2ZA==">
-                <AiFillInstagram />
-              </a>
-            </li>
-            <li>
-              {' '}
-              <a href="https://www.facebook.com/profile.php?id=61550958997339">
-                <BiLogoFacebookSquare />
-              </a>
-            </li>
-            <li>
-              {' '}
-              <a href="https://www.linkedin.com/in/code-spark-999909290">
-                <BiLogoLinkedinSquare />
-              </a>
-            </li>
-            <li>
-              {' '}
-              <a href="https://youtube.com/@CodeSpark-rr6zh?si=BmlXM50Y6Md85-Av">
-                <AiFillYoutube />
-              </a>
-            </li>
-            <li>
-              {' '}
-              <a href="https://www.tiktok.com/@codesparkua?_t=8fyiFPi3ItZ&_r=1">
-                <BiLogoTiktok />
-              </a>
-            </li>
-          </ul>
+          <SocialsList />
         </div>
         <div className="footer__form-wrapper">
           <div className="footer__form-text-wrapper">
@@ -65,7 +29,8 @@ const Footer = () => {
               Напиши нам, якщо маєш питання щодо навчання або потрібна допомога.
               Ми передзвонимо і залюбки поговоримо з тобою
             </p>
-            <p className='footer__hotfix'
+            <p
+              className="footer__hotfix"
               style={{
                 fontSize: '18px',
                 fontWeight: 'bold',
--- src/components/views/ContactView/ContactView.jsx
+++ src/components/views/ContactView/ContactView.jsx
@@ -1,18 +1,99 @@
-import ContactForm from '../../ContactForm/ContactForm';
-import ContactUsImg from '../../../images/contact-us.jpg';
+import { useState } from 'react';
+import SocialsList from '../../SocialsList';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { motion, AnimatePresence } from 'framer-motion';
+import { Link } from 'react-scroll';
 
 const ContactView = () => {
+  const [isCopied, setIsCopied] = useState(false);
+  const copyToClipboard = text => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(text);
+    } else {
+      // Если буфер обмена не поддерживается, можно использовать другой способ.
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+    }
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 6000);
+  };
+
+  const handleOnParagraphClick = event => {
+    const spanElement = event.target
+      .closest('.contact__phone')
+      .querySelector('.contact__phone-number');
+
+    const textToCopy = spanElement.textContent;
+    copyToClipboard(textToCopy);
+  };
+
   return (
     <section className="contact">
       <div className="container">
-        <h2 className="title">Зв'яжіться з нами</h2>
-        <p className="subtitle">
-          Досліджуйте майбутнє разом з нами. Не соромтеся зв'язатися.
-        </p>
-        <div className="img-wrapper">
-          <img src={ContactUsImg} width={400} alt="" />
+        <div className="contact__heading">
+          <div className="contact__heading-text">
+            <h2 className="title title--left">Контакти Online</h2>
+            <p className="subtitle subtitle--left">
+              Залишились запитання? Ось наші контакти, будемо раді вашому
+              дзвінку
+            </p>
+          </div>
+          <SocialsList className={'contact__socials-list'} />
         </div>
-        <ContactForm isMsgFieldVisible={true} />
+        <ul className="contact__variants-list">
+          <li className="contact__variants-item">
+            <p>На всі ваші запитання дадуть відповідь адміністратори</p>
+            <p onClick={handleOnParagraphClick} className="contact__phone">
+              <span className="contact__phone-icon">
+                <BsFillTelephoneFill />
+              </span>
+              <span className="contact__phone-number">093-080-6375</span>
+            </p>
+            <p onClick={handleOnParagraphClick} className="contact__phone">
+              <span className="contact__phone-icon">
+                <BsFillTelephoneFill />
+              </span>
+              <span className="contact__phone-number">099-036-7386</span>
+            </p>
+            <p>codespark.course@gmail.com</p>
+
+            <AnimatePresence>
+              {isCopied && (
+                <motion.span
+                  className="copied-status"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  // onClick={setIsCopied(false)}
+                >
+                  Номер скопійовано🎉
+                </motion.span>
+              )}
+            </AnimatePresence>
+          </li>
+          <li className="contact__variants-item">
+            <p>
+              Залиште запит на безкоштовну консультацію та наш адміністратор
+              зв'яжеться з вами сам.
+            </p>
+            <Link
+              className="contact__button"
+              to={'footer'}
+              spy={true}
+              smooth={true}
+              offset={-20}
+              duration={500}
+            >
+              Отримати консультацію
+            </Link>
+          </li>
+        </ul>
       </div>
     </section>
   );
