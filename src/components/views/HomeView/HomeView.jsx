fd08cafb018e5920f1889356da9424b1e9c477ff
Makssik <maksik07052004@gmail.com>
2023-12-30 10:38:42 +0200

    helmet

--- src/components/views/HomeView/HomeView.jsx
+++ src/components/views/HomeView/HomeView.jsx
@@ -5,49 +5,13 @@ import Reviews from '../../Reviews';
 import Hero from '../../Hero';
 import { motion } from 'framer-motion';
 import setings from '../../../extension/settingsForInitialTransition';
-import { Helmet } from 'react-helmet';
+import HomeHelmet from './HomeHelmet';
 // import faviconImg from '../../../images/favicon.ico';
 
 const HomeView = () => {
   return (
     <>
-      <Helmet>
-        <meta charset="UTF-8" />
-        <meta name="robots" content="index, follow" />
-        <meta
-          name="title"
-          content="CodeSpark: Навчимо дітей від 4 до 16 років програмувати з нуля за 4 місяці."
-        />
-        <meta
-          name="description"
-          content="codespark Безкоштовний пробний урок. Зробимо вашу дитину Python-програмістом з нуля за 4 місяці на онлайн курсах - CodeSpark. ☎️(093)080-6375. 70% наших клієнтів отримали якісну почву для старту в IT. Досвідчені викладачі які є професіоналами в своїй галузі, Ефективна програма навчання. Доступна ціна навчання. кодспарк, код спарк"
-        />
-        <meta property="og:type" content="article" />
-        <meta
-          property="og:title"
-          content="Курси програмування онлайн з нуля для дітей"
-        />
-        <meta
-          property="og:description"
-          content="codespark Безкоштовний пробний урок. Зробимо вашу дитину Python-програмістом з нуля за 4 місяці на онлайн курсах - CodeSpark. ☎️(093)080-6375. 70% наших клієнтів отримали якісну почву для старту в IT. Досвідчені викладачі які є професіоналами в своїй галузі, Ефективна програма навчання. Доступна ціна навчання. кодспарк, код спарк"
-        />
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/Makssik/codespark/main/src/images/favicon.ico"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="keywords"
-          content="CodeSpark, codespark, code spark, код спарк, кодспарк, Курси програмування, Курси програмування для дітей, Python курси для дітей, курси пайтон для дітей, Навчання програмування, візуальне програмування, Programming courses, Programming courses for children, Python courses for children, Python courses for children, Learning programming, visual programming"
-        />
-        <meta
-          name="keywords"
-          content="Обучение програмирования, Курсы для детей, Python курсы для детей, Python для детей, Python для дітей, програмування з нуля, Programming courses, Programming courses for children, Python courses for children, Python courses for children, Learning programming, visual programming"
-        />
-        <link rel="canonical" href="https://codespark.com.ua/" />
-        <meta property="url" content="https://codespark.com.ua/" />
-        <meta property="og:url" content="https://codespark.com.ua/" />
-      </Helmet>
+      <HomeHelmet />
       <motion.section {...setings}>
         <Hero
           subtitle={
